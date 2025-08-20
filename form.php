432eb4336852848f2038912af4968becb036021a
root230 <jameselectrical230@gmail.com>
2025-08-20 12:28:47 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Corpatible"content="IE=edge">
+<meta name="viewport"content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<title>ኮልፌ ደንብ ማስከበር ፅ/ቤት</title>
+
+</head>
+<body>
+<div class="container my-2">
+<table class="table">
+<thead class="thead-dark">
+<tr>
+
+<th scope="col">ክፍለ_ከተማ</th>
+<th scope="col">ወረዳ</th>
+<th scope="col">ቀጠና</th>
+<th scope="col">X_ኮርዲኔት</th>
+<th scope="col">y_ኮርዲኔት</th>
+<th scope="col">ኮድ</th>
+<th scope="col">ስፋት</th>
+
+<th scope="col">የተረከበው_አካል</th>
+<th scope="col">ምርመራ</th>
+
+</tr>
+</thead>
+<tbody>
+<?php    
+$hostname="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="testdb";
+$conn=mysqli_connect($hostname,$dbuser,$dbpass,$dbname);
+if(!$conn){
+    die("connection is not successfull");}
+    $sql="SELECT * FROM user";
+    $result=mysqli_query($conn,$sql);
+    while($row=mysqli_fetch_array($result)){
+       $ክፍለ_ከተማ=$row["ክፍለ_ከተማ"];
+        $ወረዳ=$row["ወረዳ"];
+        $ቀጠና=$row["ቀጠና"];
+           $X_ኮርዲኔት=$row["X_ኮርዲኔት"];
+           $Y_ኮርዲኔት=$row["y_ኮርዲኔት"];
+            $ኮድ=$row["ኮድ"];
+           $ስፋት=$row["ስፋት"];
+          
+          
+           $የተረከበውአካል=$row["የተረከበው_አካል"];
+           $ምርመራ=$row["ምርመራ"];
+              
+              echo"<tr>
+              <td>$ክፍለ_ከተማ</td>
+              <td>$ወረዳ</td>
+              <td>$ቀጠና</td>
+              <td>$X_ኮርዲኔት</td>
+              <td>$Y_ኮርዲኔት</td>
+              <td>$ኮድ</td>
+              <td>$ስፋት</td>
+              <td>$የተረከበውአካል</td>
+              <td>$ምርመራ</td>
+             
+              </tr>";
+    
+
+    }
+
+
+?>
+</tbody>
+</div>
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>submit form</title>
+</head>
+
+<body>
+<h2>ኮልፌ ቀራንዮ ክፍለ-ከተማ ደንብ ማስከበር ፅ/ቤት ተረክቦ የሚጠብቃቸው መሬት ባንክ የገቡ ቦታዎች የሚገኙበት አድራሻ የሚያሳይ መረጃ</h2>
+<form action="submit.php" method="post">
+ክፍለ_ከተማ:<input type="text"name="ክፍለ_ከተማ"required><br><br>
+ወረዳ:<input type="text"name="ወረዳ"required><br><br>
+ቀጠና:<input type="text"name="ቀጠና"required><br><br>
+
+X_ኮርዲኔት:<input type="text"name="X_ኮርዲኔት"required><br><br>
+y_ኮርዲኔት:<input type="text"name="y_ኮርዲኔት"required><br><br>
+ስፋት<input type="text"name="ስፋት"required><br><br>
+ኮድ:<input type="text"name="ኮድ"required><br><br>
+የተረከበው_አካል:<input type="text"name="የተረከበው_አካል"required><br><br>
+ምርመራ:<input type="text"name="ምርመራ"required><br><br>
+<input type="submit" value="ላክ">
+</form>
+</body>
+</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$id = $_POST['id'] ?? '';
+$ክፍለ_ከተማ = $_POST['ክፍለ_ከተማ'] ?? '';
+$ወረዳ = $_POST['ወረዳ'] ?? '';
+$ቀጠና = $_POST['ቀጠና'] ?? '';
+
+$X_ኮርዲኔት = $_POST['X_ኮርዲኔት'] ?? '';
+$y_ኮርዲኔት = $_POST['y_ኮርዲኔት'] ?? '';
+$ስፋት = $_POST['ስፋት'] ?? '';
+$ኮድ = $_POST['ኮድ'] ?? '';
+$የተረከበው_አካል = $_POST['የተረከበው_አካል'] ?? '';
+$ምርመራ = $_POST['ምርመራ'] ?? '';
+if (empty($ወረዳ) || empty($ቀጠና)) { die("Error: ወረዳ and ቀጠና are required."); }
+
+$sql = "INSERT INTO user (ክፍለ_ከተማ,ወረዳ,ቀጠና,X_ኮርዲኔት,y_ኮርዲኔት,ስፋት,ኮድ,የተረከበው_አካል,ምርመራ) VALUES (?,?,?,?,?,?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssssssss",$ክፍለ_ከተማ,$ወረዳ, $ቀጠና,$X_ኮርዲኔት,$y_ኮርዲኔት,$ስፋት,$ኮድ,$የተረከበው_አካል,$ምርመራ);
+
+if ($stmt->execute()) {
+    echo "መረጃዎ በትክክል ተመዝግቧል.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$conn->close();
+?>
